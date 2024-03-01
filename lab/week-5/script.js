dc5352069bfdd887a846b66eab36f95f5108b0c8
songh194 <songh194@newschool.edu>
2024-03-01 11:41:01 -0500

    labinclass

---
+++ lab/week-5/script.js
@@ -0,0 +1,10 @@
+const circle = document.querySelector(".js-circle");
+
+circle.addEventListener("mouseenter",function ()) {
+    if (circle.classList.contains("red")) {
+        //do nothingggg 
+    } else {
+        //change to red 
+        circle.classList.add("red");
+    }
+}
