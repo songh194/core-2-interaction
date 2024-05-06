f6eebaf2e373287aa86b53857f91e13195e0e32e
songh194 <songh194@newschool.edu>
2024-05-06 09:54:33 -0400

    p3

--- studio/project-3/script.js
+++ studio/project-3/script.js
@@ -1,57 +1,3 @@
-// time
-function updateTime () {
-    var today = new Date();
-    var hour = today.getHours();
-    var minute = today.getMinutes();
-    var prepand = (hour >= 12)? " PM " : " AM ";
-
-    hour = (hour >=12)? hour - 12 : hour;
-
-    var currentTime = (hour + ":" + minute + prepand) 
-    document.querySelector('#time').textContent = currentTime; 
-}
-setInterval(updateTime,1000);
-
-// let progressBar = [
-//   {'stop1':'google.com'},
-//   {'stop2':'maps.google.com'}
-// ]
-
-// var i = 0;
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.getElementById("myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
-
-
-// var eachStop = document.querySelector('.stop');
-
-// function bringEachStop(){
-//   var eachStop = document.getElementsByClassName('.stop')
-// }
-// let dots = document.getElementsByClassName('.stop');
-// let i = 0;
-// while (i<dots.length) {
-//   console.log(dots[i])
-//   i++
-// }
-
-
-// bring dots in, loop
-// change color
 
 const progressBarContainer = document.getElementById('pbc');
 const dots = progressBarContainer.querySelectorAll('.stop');
@@ -63,39 +9,99 @@ function changeDotColor(index) {
     dots[index].style.backgroundColor = 'orange'; 
     setTimeout(() => {
         changeDotColor(index + 1); 
-    }, 20000);
+    }, 100000);
 }
 changeDotColor(0);
 
-// change iframe as set time, "open and close door"
-// link 'stops',,,= { stop name: url }
+
 
 let stops = [
-  {'googleMaps': "https://www.google.com/maps"},
-  {'google':"https://www.google.com"},
-  {'wallmart':'https://www.walmart.com'},
-  {'nfl':'https://www.nfl.com'},
-  {'googleTranslate':'https://translate.google.com'},
-  {'weather':'https://www.weather.com'},
-  {"instagram":'https://www.instagram.com'},
-  {"chatGPT":'https://chat.openai.com'},
-  {'whatsApp':'https://www.whatsapp.com'},
-  {'amazon':'https://www.amazon.com'},
-  {'twitter':'https://twitter.com'},
-  {'facebook':'https://www.facebook.com'},
-  {'gmail':'https://mail.google.com'},
-  {'netflix':'https://www.netflix.com'},
-  {'wordle':'https://www.nytimes.com/games/wordle'},
-  {'weather':'https://www.weather.com'},
-  {'youtube':'https://www.youtube.com'},
-  {'wilipedia':'https://www.wikipedia.org'},
-  {'tiktok':'https://www.tiktok.com'},
-  {'yahoo':'https://www.yahoo.com'}
+  {'Google Maps': "https://www.google.com/maps"},
+  {'Google':"https://www.google.com"},
+  {'Wallmart':'https://www.walmart.com'},
+  {'NFL':'https://www.nfl.com'},
+  {'Google Translate':'https://translate.google.com'},
+  {'Weather':'https://www.weather.com'},
+  {"Instagram":'https://www.instagram.com'},
+  {"ChatGPT":'https://chat.openai.com'},
+  {'WhatsApp':'https://www.whatsapp.com'},
+  {'Amazon':'https://www.amazon.com'},
+  {'Twitter':'https://twitter.com'},
+  {'Facebook':'https://www.facebook.com'},
+  {'Gmail':'https://mail.google.com'},
+  {'Netflix':'https://www.netflix.com'},
+  {'Wordle':'https://www.nytimes.com/games/wordle'},
+  {'Reddit':'https://www.reddit.com'},
+  {'YouTube':'https://www.youtube.com'},
+  {'Wikipedia':'https://www.wikipedia.org'},
+  {'TikTok':'https://www.tiktok.com'},
+  {'Yahoo':'https://www.yahoo.com'}
 ]
 
-function displayStop(item) {
-  let website = document.createElement('stop')
-  website.src = item.values;
-  website.classList.add("frameStyle");
-  document.getElementsByClassName('mainContainer')
+function displayStops() {
+  const stopContainer = document.querySelector('.mainContainer');
+  stopContainer.innerHTML = '';
+
+  const currentIndex = displayStops.index || 0;
+  const currentStop = stops[currentIndex];
+  const key = Object.keys(currentStop)[0];
+  const value = currentStop[key];
+
+  const stopKeyElement = document.createElement('div');
+  stopKeyElement.textContent = key;
+
+  const iframeElement = document.querySelector('.iframe iframe');
+  iframeElement.src = value;
+
+  stopContainer.appendChild(stopKeyElement);
+
+  displayStops.index = (currentIndex + 1) % stops.length;
+
+  const blinkDuration = 500; // milliseconds
+  const blinkCount = 20; // Number of times to blink
+  let blinkToggle = true;
+  let blinkInterval;
+  blinkInterval = setInterval(() => {
+    if (blinkToggle) {
+      stopKeyElement.style.fontFamily = '"bitcount-mono-single-square", sans-serif';
+    } else {
+      stopKeyElement.style.fontFamily = '"bitcount-mono-single-line-sq", sans-serif';
+    }
+    blinkToggle = !blinkToggle;
+    blinkCount--;
+    if (blinkCount === 0 || linkElement.style.pointerEvents === "none") {
+      clearInterval(blinkInterval);
+    }
+  }, blinkDuration);
+
+  setTimeout(() => {
+    clearInterval(blinkInterval); // Stop blinking animation after 5 seconds
+    stopKeyElement.style.fontFamily = '"Yarndings 12", system-ui'; // Reset to default font
+    stopKeyElement.classList.add('inbetweenStops'); // Apply the 'inbetweenStops' class
+  }, 5000);
 }
+
+displayStops(0);
+
+
+
+
+
+
+//pop up 
+let overlay = document.getElementById("overlay");
+let popup = document.getElementById("popup");
+
+function closePopup() {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function closeAd() {
+    let ads = document.getElementsByClassName("ad");
+    for (let i = 0; i < ads.length; i++) {
+        ads[i].classList.add("closedAd");
+    }
+}
+
+
