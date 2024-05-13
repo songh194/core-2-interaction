06227ff9724c5da4c7b82af48454ccef21dd7ad7
songh194 <songh194@newschool.edu>
2024-05-13 12:08:40 -0400

    skills test

---
+++ lab/skills-test/script.js
@@ -0,0 +1,31 @@
+function add(x,y) {
+    var result = x + y;
+    console.log(result);
+}
+
+function makeItGreen() {
+    const blueCircle = document.getElementById('circle')
+    blueCircle.classList.add('green')
+
+}
+
+let spongebob = {
+    "name": "spongebob",
+    "pet":"gary",
+    "job": "cook",
+    "city":"bikini bottom",
+    "birth year":1986,
+    "friends": [
+        {"name": "patrick", "house":"under a rock"},
+        {"name": "squidward", "house":"moai"},
+        {"name": "sandy","house":"treedome"}
+    ],
+    "hobbies":["jellyfish fishing","practicing karate with sandy","blowing bubbles"]
+}
+
+console.log(spongebob)
+
+
+
+
+
